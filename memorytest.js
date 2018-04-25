0e0759b2f4d6b082aa9aa1c397308db410e9de69
Jorf <dontstutteronmylastname@gmail.com>
2018-04-24 22:23:08 -0500

    refactored and shortened internal function

--- memorytest.js
+++ memorytest.js
@@ -1,18 +1,18 @@
 function memoryDist(arr){
   var arrDuplicateCheck = [];
-  var largestNum = Math.max(...arr);
-  var largestNumIndex = arr.indexOf(largestNum);
-  var startIndex = nextIndex(largestNumIndex);
 
   function nextIndex(ind){
     return ind == 3 ? 0 : ind + 1;
   }
 
-  arr[largestNumIndex] = 0;
+  function addToEls(originalArr) {
+    var largestNum = Math.max(...originalArr);
+    var largestNumIndex = originalArr.indexOf(largestNum);
 
-  function addToEls(originalArr, newIndex, counter) {
-    let a = newIndex;
-    for (var i = counter; i > 0; i--) {
+    var startIndex = nextIndex(largestNumIndex);
+    let a = startIndex;
+    originalArr[largestNumIndex] = 0;
+    for (var i = largestNum; i > 0; i--) {
       originalArr[a] =  originalArr[a] + 1;
       if (a == 3) {
         a = 0;
@@ -20,12 +20,8 @@ function memoryDist(arr){
         a += 1;
       }
     }
-    arrDuplicateCheck.push(originalArr);
-    return originalArr;
+    return originalArr
  }
-
-
-
 }
 var test = [2, 4, 1, 2];
 memoryDist(test);
